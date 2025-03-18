e1ccc88e6fb340f3674736c1b4cf5855be43a219
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2025-03-18 10:01:09 -0500

    backend 3 parte 1

--- routes/fotos.js
+++ routes/fotos.js
@@ -1,12 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-const Sequelize = require('sequelize');
+const {Sequelize, Op, where} = require('sequelize');
 const { route } = require('.');
+const foto = require('../models/foto');
 const Foto = require('../models').foto;
 const Etiqueta = require('../models').etiqueta; 
 
 
+router.get('/findAllbyRate/json', function(req,res,next){
+    let lower = parseFloat(req.query.lower);
+    let higher = parseFloat(req.query.higher);
+    Foto.findAll({
+        attributes: {exclude: ["updatedAt"]},
+        include:[{
+            model: Etiqueta,
+            attributes: ['texto'],
+                through: {attributes:[]},
+        }],
+        where: {
+            calificacion:{
+                [Op.between]:[lower, higher]
+            }
+        }
+    })
+    .then(fotos => {
+        res.json(fotos);
+    })
+    .catch(error => res.status(400).send(error))
+})
+
+router.get('/findAllById/:id/json', function(req,res,next){
+    let id = parseInt(req.params.id);
+    Foto.findAll({
+        attributes: {exclude: ["updatedAt"] },
+        include: [{
+            model: Etiqueta,
+            attributes: ['texto'],
+            through: {attributes: []}
+        }],
+        where:{
+            [Op.and]: {id: id}
+        }
+    })
+    .then(fotos => {res.json(fotos);
+    })
+    .catch(error => tes.status(400).send(error))
+})
 
 router.get('/findAll/json', function(req, res, next) {
     Foto.findAll({
