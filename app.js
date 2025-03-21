c456d96cb42a0a8101c7e0620a6b9260b0873ab3
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2025-03-20 23:03:58 -0500

    backend 4 formularios

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fotosRouter = require('./routes/fotos');
+var restRouter = require('./routes/rest/fotos');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fotos', fotosRouter);
+app.use('/rest/fotos', restRouter);
 
 
 // catch 404 and forward to error handler
